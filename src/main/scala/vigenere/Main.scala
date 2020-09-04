f98807f4e1a2e20461dba8d345efce7920f37a12
Illya Datsyuk <ilya.datsyuk@gmail.com>
2020-09-04 12:14:50 +0300

    Add message encryptor

--- src/main/scala/vigenere/Main.scala
+++ src/main/scala/vigenere/Main.scala
@@ -4,16 +4,20 @@ import scala.annotation.tailrec
 import scala.io.StdIn
 
 object Main extends App {
+
   print("Enter your message: ")
-  val plaintext = StdIn.readLine().trim
+  val plaintext = StdIn.readLine().trim.toUpperCase
 
   print("Enter your key: ")
-  val key = StdIn.readLine().trim
+  val key = StdIn.readLine().trim.toUpperCase
 
-  print(s"Elongated key: ${Helpers.elongateKey(key, plaintext.length)}")
+  println(s"Encrypted message: ${Helpers.encryptMessage(Helpers.elongateKey(key, plaintext.length), plaintext)}")
 }
 
 private object Helpers {
+
+  private val indexedAlphabet = (0 to 25).zip('A' to 'Z').toMap
+
   def elongateKey(key: String, messageLength: Int): String = {
 
     if (key.length >= messageLength) key
@@ -22,8 +26,7 @@ private object Helpers {
 
       @tailrec
       def elongateHelper(index: Int = 0, keyToElongate: String): String = {
-
-        if (index.equals(charactersToAppendCount)) keyToElongate
+        if (index == charactersToAppendCount) keyToElongate
         else {
           val newKey = keyToElongate.appended(key.charAt(index % key.length))
           elongateHelper(index + 1, newKey)
@@ -32,5 +35,20 @@ private object Helpers {
 
       elongateHelper(keyToElongate = key)
     }
+  }
+
+  def encryptMessage(key: String, message: String): String = {
+
+    @tailrec
+    def cipherHelper(index: Int = 0, ciphertext: String = ""): String = {
+
+      if (index == message.length) ciphertext
+      else {
+        val encryptedChar = indexedAlphabet((message.charAt(index) + key.charAt(index)) % 26)
+        cipherHelper(index + 1, ciphertext.appended(encryptedChar))
+      }
+    }
+
+    cipherHelper()
   }
 }
